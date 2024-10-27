0fa68788e8256194e1f91e1c23661f93d2e38a72
Kimutai101 <tiropgarreth@gmail.com>
2024-10-27 06:12:47 +0300

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Initial commit 
+This is a small program on how to consume the stackoverflow API and return all the questions and links.
+- Returns all the questions with an answer_count of less than 5 
--- consume_api.py
+++ consume_api.py
@@ -1,2 +1,32 @@
 import requests
 import json
+
+response1 = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+#print(response1)
+# [200] means response was successful and the specific resource was returned.
+#print(response1.json())
+
+#Explore items specifically
+
+#print(response1.json()['items'])
+
+#To print the specific question and links in each item
+
+'''for data_x in response1.json()['items']:
+    print(data_x['title'])
+    print(data_x['link'])
+    print(' ')
+    '''
+    
+#Too print those questions where the answer count is less than 5
+
+for data_x in response1.json()['items']:
+    if data_x['answer_count'] < 5:  
+        print(data_x['title'])
+        print(data_x['link'])
+        print(' ')
+    
+    else:
+        print("skipped") # For those with more than 5 answers
+    
+    
